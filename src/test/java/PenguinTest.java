9a59ccfef3f1cfebb80c4bc86c4fdacb44cb12dc
NathanZC <nathanccox52@gmail.com>
2021-09-27 17:54:11 -0400

    fixed naming convention thingy and comment

--- src/test/java/PenguinTest.java
+++ src/test/java/PenguinTest.java
@@ -1,6 +1,5 @@
 /*
- * This file contains sample JUnit test cases for Horse.java
- * You should NOT modify this file!
+
  */
 
 import org.junit.*;
@@ -9,38 +8,38 @@ import static org.junit.Assert.*;
 
 
 public class PenguinTest {
-    Penguin Penguin1;
+    Penguin p1;
 
     @Before
     public void setUp() throws Exception {
-        Penguin1 = new Penguin();
+        p1 = new Penguin();
     }
 
     @Test(timeout = 50)
     public void TestSound() {
-        assertEquals("splat!", Penguin1.sound());
+        assertEquals("splat!", p1.sound());
     }
 
     @Test(timeout = 50)
     public void TestGetMaxSpeed() {
-        assertEquals(3, Penguin1.getMaxSpeed());
+        assertEquals(3, p1.getMaxSpeed());
     }
 
     @Test(timeout = 50)
     public void TestUpgradeSpeed() {
-        Penguin1.upgradeSpeed();
-        assertEquals(4, Penguin1.getMaxSpeed());
+        p1.upgradeSpeed();
+        assertEquals(4, p1.getMaxSpeed());
     }
 
     @Test(timeout = 50)
     public void TestDowngradeSpeed() {
-        Penguin1.downgradeSpeed();
-        assertEquals(2, Penguin1.getMaxSpeed());
+        p1.downgradeSpeed();
+        assertEquals(2, p1.getMaxSpeed());
     }
 
     @Test(timeout = 50)
     public void TestGetPrice() {
-        assertEquals(20, Penguin1.getPrice());
+        assertEquals(20, p1.getPrice());
     }
 
 }
