18187d80944c55a1d933fc1c5310c0817da9f3a1
marian-wang <84052919+marian-wang@users.noreply.github.com>
2021-09-27 17:12:23 -0400

    finished lab 3 with new Tesla class and test

---
+++ src/main/java/Tesla.java
@@ -0,0 +1,32 @@
+public class Tesla implements Drivable, Tradable {
+    private int maxSpeed;
+
+    public Tesla() {
+        this.maxSpeed = 100;
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        this.maxSpeed += 5;
+    }
+
+    @Override
+    public void downgradeSpeed() {
+        this.maxSpeed -= 5;
+    }
+
+    @Override
+    public int getPrice() {
+        return 5;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "A Beautiful Tesla with Max Speed (" + this.maxSpeed + "mph)";
+    }
+}
---
+++ src/test/java/TeslaTest.java
@@ -0,0 +1,47 @@
+/*
+ * This file contains sample JUnit test cases for Horse.java
+ * You should NOT modify this file!
+ */
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+public class TeslaTest {
+    Tesla t;
+
+    @Before
+    public void set_up() throws Exception {
+        t = new Tesla();
+    }
+
+    @Test(timeout = 50)
+    public void test_get_max_speed() {
+        assertEquals(100, t.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void test_upgrade_speed() {
+        t.upgradeSpeed();
+        assertEquals(105, t.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void test_downgrade_speed() {
+        t.downgradeSpeed();
+        assertEquals(95, t.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void test_get_price() {
+        assertEquals(5, t.getPrice());
+    }
+
+    @Test(timeout = 100)
+    public void test_to_string() {
+        String expected = "A Beautiful Tesla with Max Speed (100mph)";
+        assertEquals(expected, t.toString());
+    }
+
+}
