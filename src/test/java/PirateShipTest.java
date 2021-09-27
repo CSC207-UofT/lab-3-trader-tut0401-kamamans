c0ef66a8dd2c72d8cdb4a2474ac269e57e30d054
zhaoand9 <ae.zhao@mail.utoronto.ca>
2021-09-27 17:22:09 -0400

    Ship

---
+++ src/test/java/PirateShipTest.java
@@ -0,0 +1,41 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+public class PirateShipTest {
+    PirateShipTest p;
+
+    @Before
+    public void setUp() throws Exception {
+        p = new PirateShipTest();
+    }
+
+    @Test(timeout = 50)
+    public void TestSound() {
+        assertEquals("Ahoy!", p.sound());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetMaxSpeed() {
+        assertEquals(696969, p.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestUpgradeSpeed() {
+        p.upgradeSpeed();
+        assertEquals(697038, p.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestDowngradeSpeed() {
+        p.downgradeSpeed();
+        assertEquals(696900, p.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetPrice() {
+        assertEquals(17, p.getPrice());
+    }
+
+}
