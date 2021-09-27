4b8f3251578d63357bd56db7be64d937c025e933
Silk8Worm <60560778+Silk8Worm@users.noreply.github.com>
2021-09-27 17:37:35 -0400

    Update WagonTest.java

--- src/test/java/WagonTest.java
+++ src/test/java/WagonTest.java
@@ -1,6 +1,5 @@
 /*
- * This file contains sample JUnit test cases for Horse.java
- * You should NOT modify this file!
+ * This file contains JUnit test cases for the Wagon class.
  */
 
 import org.junit.*;
